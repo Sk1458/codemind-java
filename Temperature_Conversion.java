c77d5f849da232723e3e719aa4ba6f173cef4068
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-10 21:29:18 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner scan = new Scanner(System.in);         double c = scan.nextDouble();         System.out.format("%.2f",32+(c*1.8));     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        double c = scan.nextDouble();
+        System.out.format("%.2f",32+(c*1.8));
+    }
+}
