2baf465ce2c08a5f7fc247d1272b416aedf6d07f
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-11 21:43:57 +0530

    import java.util.*; class Unicron { 	public static void main(String args[]) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int[] arr = new int[n]; 		int c = 0 ; 		for(int i = 0 ; i < n ; i++) 		{ 		    arr[i] = sc.nextInt(); 		    if(arr[i]%2==0) 		    { 		        c=i; 		    } 		} 		System.out.format("%d",c); 	} }

---
+++ Last_even_element_index.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Unicron
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int c = 0 ;
+		for(int i = 0 ; i < n ; i++)
+		{
+		    arr[i] = sc.nextInt();
+		    if(arr[i]%2==0)
+		    {
+		        c=i;
+		    }
+		}
+		System.out.format("%d",c);
+	}
+}
