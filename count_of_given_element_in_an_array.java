06d163c59a996fa671147fafd07c7b1d91fedd4b
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-11 21:59:24 +0530

    import java.util.*; class Ratchet { 	public static void main(String args[]) 	{ 		Scanner sc = new Scanner(System.in); 		int n = sc.nextInt(); 		int[] arr = new int[n]; 		int c = 0 ; 		for(int i = 0 ; i < n ; i++) 		{ 		    arr[i] = sc.nextInt(); 		} 		int k = sc.nextInt(); 		for(int i = 0 ; i < n ; i++) 		{ 		    if(arr[i]==k) 		    { 		        c++; 		    } 		} 		System.out.format("%d",c); 	} }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Ratchet
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int c = 0 ;
+		for(int i = 0 ; i < n ; i++)
+		{
+		    arr[i] = sc.nextInt();
+		}
+		int k = sc.nextInt();
+		for(int i = 0 ; i < n ; i++)
+		{
+		    if(arr[i]==k)
+		    {
+		        c++;
+		    }
+		}
+		System.out.format("%d",c);
+	}
+}
